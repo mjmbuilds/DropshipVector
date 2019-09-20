b4abf277e07f040828a8278dafaf6aaef83c5981
MJM <40216077+mjmbuilds@users.noreply.github.com>
2019-09-20 11:25:16 -0400

    Checkin

--- Source/TEST25/TEST25.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TEST25.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TEST25, "TEST25" );
